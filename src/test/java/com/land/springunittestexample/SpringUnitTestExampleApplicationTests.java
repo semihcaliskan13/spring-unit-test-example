afd8bbedcc52d7dc5a46d7b459f3b5e2d236cb8d
semih <semihcaliskan0103@gmail.com>
2024-01-01 00:41:45 +0300

    Initial commit

---
+++ src/test/java/com/land/springunittestexample/SpringUnitTestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.land.springunittestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUnitTestExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
